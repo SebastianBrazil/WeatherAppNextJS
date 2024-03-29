75768a7b406af1a1f4b00fe1479c0bdbf2df4175
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-03-29 10:24:45 -0700

    vercel plzz PT2

--- src/DataServices/DataServices.ts
+++ src/DataServices/DataServices.ts
@@ -1,19 +1,19 @@
 export const getCities = async (userInput: string) => {
-    const promise = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${process.env.NEXT_PUBLIC_WEATHER_APIKEY}`)
+    const promise = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`)
     const data = await promise.json();
 
     return data;
 }
 
 export const getCurWea = async (lat: string, lon: string, tempType: string) => {
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_APIKEY}&units=${tempType}`)
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_ANALYTICS_ID}&units=${tempType}`)
     const data = await promise.json();
 
     return data;
 }
 
 export const getForcastWea = async (lat: string, lon: string, tempType: string) => {
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_APIKEY}&units=${tempType}`)
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_ANALYTICS_ID}&units=${tempType}`)
     const data = await promise.json();
 
     return data.list;
